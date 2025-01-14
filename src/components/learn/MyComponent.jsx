c35453fe7a877e95fda5ae7b0f5cd22edab811e3
Tonyvisko <vietthanh0135@gmail.com>
2025-01-15 00:35:10 +0700

    video 30

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -1,8 +1,15 @@
 // () => { }
 //component = html + css + js
+
+import './style.css';
 const MyComponent = () => {
     return (
-        <div> eric & hoidanit update </div>
+        <>
+            <div> eric & hoidanit update </div> //tag
+            <div className="child"
+                style={{ borderRadius: "10px" }}
+            >child</div>
+        </>
     );
 }
 
