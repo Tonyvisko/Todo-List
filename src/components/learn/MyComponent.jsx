812fc77294ceb2094085e1cd6801d9a9263872a1
Tonyvisko <vietthanh0135@gmail.com>
2025-01-15 00:48:41 +0700

    video 31

--- src/components/learn/MyComponent.jsx
+++ src/components/learn/MyComponent.jsx
@@ -3,11 +3,24 @@
 
 import './style.css';
 const MyComponent = () => {
+    // const hoidanit = "eric"; //string
+    // const hoidanit = 25; //number
+    // const hoidanit = true; //boolean
+    // const hoidanit = undefined;
+    // const hoidanit = null;
+    const hoidanit = [1, 2, 3]
+    // const hoidanit = {
+    //     name: "hoidanit",
+    //     age: 25
+    // }
     return (
         <>
-            <div> eric & hoidanit update </div> //tag
+            <div> {JSON.stringify(hoidanit)} & hoidanit update </div> //tag
+            <div>{console.log("ERIC")}</div>
             <div className="child"
-                style={{ borderRadius: "10px" }}
+                style={
+                    { borderRadius: "10px" }
+                }
             >child</div>
         </>
     );
