59a4c81200ff7f4ccefd57e53713a95a94800e49
Tonyvisko <vietthanh0135@gmail.com>
2025-02-03 01:15:14 +0700

    video 35

--- src/components/todo/TodoNew.jsx
+++ src/components/todo/TodoNew.jsx
@@ -1,5 +1,8 @@
 
-const TodoNew = () => {
+const TodoNew = (props) => {
+    console.log(">>> check point: ", props)
+    const { addNewTodo } = props;
+    // addNewTodo("Eric")
     return (
         <div className="todo-new">
             <input type="text"></input>
