97ba8b2426f74101fd0fe1ff3857b6eaada51f15
Tonyvisko <vietthanh0135@gmail.com>
2025-02-03 00:34:42 +0700

    video 34

--- src/components/todo/TodoData.jsx
+++ src/components/todo/TodoData.jsx
@@ -1,7 +1,21 @@
 
-const TodoData = () => {
+const TodoData = (props) => {
+    //props là một object {}
+    // {
+    //     name: "Eric",
+    //     age: 25,
+    //     data: {}
+    // }
+    //C1:
+    const { name, age, data } = props;
+    //C2:
+    // const name = props.name;
+    // const age = props.age;
+    // const data = props.data;
+    // console.log(">>> check props: ", props)
     return (
         <div className="todo-data">
+            <div>My name is {name} </div>
             <div > Learning React</div>
             <div>Watching Youtube</div>
         </div>
