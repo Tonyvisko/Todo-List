ba596ba61350d260edd4588c7c47541acc91ba9b
Tonyvisko <vietthanh0135@gmail.com>
2025-02-05 00:54:17 +0700

    video 36

--- src/components/todo/TodoNew.jsx
+++ src/components/todo/TodoNew.jsx
@@ -2,11 +2,21 @@
 const TodoNew = (props) => {
     console.log(">>> check point: ", props)
     const { addNewTodo } = props;
-    // addNewTodo("Eric")
+    // addNewTodo("Eric") //fire
+    const handleClick = () => {
+        alert("click me")
+    }
+    const handleOnChange = (name) => {
+        console.log(">>>handleOnChange", name)
+    }
     return (
         <div className="todo-new">
-            <input type="text"></input>
-            <button>Add</button>
+            <input type="text"
+                onChange={(event) => handleOnChange(event.target.value)}// muon thuc thi fn thi boc no vao mot cai arrow fn () => ..
+            ></input>
+            <button style={{ cursor: "pointer" }}
+                onClick={handleClick}
+            >Add</button>
         </div>
     )
 }
