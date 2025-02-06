ba1a04a07e804bebaa248f433a02c573b7cee95f
Tonyvisko <vietthanh0135@gmail.com>
2025-02-06 23:09:09 +0700

    video 40

--- src/components/todo/TodoData.jsx
+++ src/components/todo/TodoData.jsx
@@ -12,20 +12,16 @@ const TodoData = (props) => {
     // const name = props.name;
     // const age = props.age;
     // const data = props.data;
-    console.log(">>> check props: ", todoList)
     return (
         <div className="todo-data">
             {todoList.map((item, index) => {
                 console.log(">>> check map: ", item, index)
                 return (
-                    <div className="todo-item">
+                    <div className={`todo-item ${item.id}`} key={item.id}>
                         <div>{item.name}</div>
                         <button style={{ cursor: "pointer" }}>Delete</button>
                     </div>)
             })}
-            <div>
-                {JSON.stringify(props.todoList)}
-            </div>
         </div>
     )
 }
