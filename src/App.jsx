cd0a125368e72d9718a9918c7dab46c14a41594e
Tonyvisko <vietthanh0135@gmail.com>
2025-02-19 22:56:19 +0700

    video 47

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,8 @@ import TodoData from './components/todo/TodoData';
 import TodoNew from './components/todo/TodoNew';
 import reactLogo from './assets/react.svg';
 import { useState } from 'react';
+import Header from './components/layout/header';
+import Footer from './components/layout/footer';
 const App = () => {
 
   const [todoList, setTodoList] = useState([
@@ -33,22 +35,25 @@ const App = () => {
 
   //{key:value}
   return (
-    <div className="todo-container">
-      <div className="todo-title">Todo List</div>
-      <TodoNew
-        addNewTodo={addNewTodo}
-      />
-      {todoList.length > 0 ?
-        <TodoData
-          todoList={todoList}
-          deleteTodo={deleteTodo}
+    <>
+      <Header />
+
+      <div className="todo-container">
+        <div className="todo-title">Todo List</div>
+        <TodoNew
+          addNewTodo={addNewTodo}
         />
-        :
-        <div className='todo-image'>
-          <img src={reactLogo} className='logo' />
-        </div>
-      }
-      {/* {todoList.length > 0 &&
+        {todoList.length > 0 ?
+          <TodoData
+            todoList={todoList}
+            deleteTodo={deleteTodo}
+          />
+          :
+          <div className='todo-image'>
+            <img src={reactLogo} className='logo' />
+          </div>
+        }
+        {/* {todoList.length > 0 &&
         <TodoData
           todoList={todoList}
         />
@@ -58,7 +63,9 @@ const App = () => {
           <img src={reactLogo} className='logo' />
         </div>
       } */}
-    </div>
+      </div>
+      <Footer />
+    </>
   )
 }
 
