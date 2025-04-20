f31048b9e648f0516bfed30d8534434aca828217
Tonyvisko <vietthanh0135@gmail.com>
2025-04-21 01:01:20 +0700

    video 67

--- src/pages/users.jsx
+++ src/pages/users.jsx
@@ -2,7 +2,7 @@ import UserForm from "../user/user.form";
 import UserTable from "../user/user.table";
 const UserPage = () => {
     return (
-        <div style={{ padding: "20px" }}>
+        <div style={{ padding: "20px " }}>
             <UserForm />
             <UserTable />
         </div>
