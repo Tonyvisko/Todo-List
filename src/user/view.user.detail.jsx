a0cc1a07dd9d323ca674e46f7c68d03f0321b7a5
Tonyvisko <vietthanh0135@gmail.com>
2025-08-10 21:41:55 +0700

    video 82

--- src/user/view.user.detail.jsx
+++ src/user/view.user.detail.jsx
@@ -1,11 +1,11 @@
-import { Drawer } from "antd";
-import { Button } from "antd/es/radio";
+import { Drawer, Button } from "antd";
 const ViewUserDetail = (props) => {
     const { dataDetail, setDataDetail, isDetailOpen, setIsDetailOpen } = props;
 
     return (
         <>
             <Drawer
+                width={"40vw"}
                 title="Chi tiết User"
                 closable={{ 'aria-label': 'Close Button' }}
                 onClose={() => { setIsDetailOpen(false), setDataDetail(null) }}
@@ -20,13 +20,31 @@ const ViewUserDetail = (props) => {
                     <br />
                     <p>Phone number: {dataDetail.phone}</p>
                     <br />
+                    <p>Avatar:</p>
+                    <div>
+                        <img height={100} width={150}
+                            src={`${import.meta.env.VITE_BACKEND_URL}/images/avatar/${dataDetail.avatar}`} />
+                    </div>
+                    <div>
+                        <label htmlFor="btnUpload" style={{
+                            display: "block",
+                            width: "fit-content",
+                            marginTop: "15px",
+                            padding: "5px 15px",
+                            background: "orange",
+                            borderRadius: "5px",
+                            cursor: "pointer"
+                        }}>
+                            Upload Avatar </label>
+                        <input type="file" hidden id='btnUpload' />
+                    </div>
                 </>
                     :
                     <>
                         <p>Không có dữ liệu</p>
                     </>
                 }
-            </Drawer>
+            </Drawer >
         </>
     );
 }
